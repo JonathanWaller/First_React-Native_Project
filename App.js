70c276d3e21628b2a1f4b1116b8333ab531a0505
JonathanWaller <waller.j27@gmail.com>
2018-11-01 21:03:54 -0500

    added image

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri:
+        "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12193133/German-Shepherd-Puppy-Fetch.jpg"
+    };
+
     return (
       <View style={styles.container}>
-        <Text>Test for JW!!!</Text>
+        <Text>Hello World!</Text>
+        <Text>Another Test</Text>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
       </View>
     );
   }
